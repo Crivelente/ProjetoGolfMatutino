cc536821dde8c51007dae83dfb44b36e45e7f038
morgenstern28 <jmedeiros@itelinfo.com>
2021-12-16 10:38:53 -0400

    Marca Repository criado

---
+++ src/main/java/com/example/projetoGolf/repositories/MarcaRepository.java
@@ -0,0 +1,10 @@
+package com.example.projetoGolf.repositories;
+
+import com.example.projetoGolf.entities.Marca;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarcaRepository extends JpaRepository<Marca, Integer> {
+
+}
